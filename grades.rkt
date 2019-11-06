aa567850665b26ad053bf0d5c3e8028f217d8641
HeisenbergSongYi <44544854+HeisenbergSongYi@users.noreply.github.com>
2019-11-06 16:21:59 -0500

    Update grades.rkt

--- grades.rkt
+++ grades.rkt
@@ -1,6 +1,4 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname grades) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
+
 (define (final-cs135-grade mt1 mt2 fe ass)
   (+(+(+(+(* mt1 0.1) (* mt2 0.15)) (* 0.5 fe) )(* ass 0.2))5))
 
